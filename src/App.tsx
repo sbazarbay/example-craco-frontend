df5587af8368aaba3d5553253440a560cc16276a
Cheena <c.bazarbai@gmail.com>
2024-08-09 10:25:44 +0500

    chore: reorganise repo, mount context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/logo.svg";
+import "./css/App.css";
+import { useContext } from "react";
+import { AuthContext } from "./AuthContext";
 
 function App() {
+  const authContext = useContext(AuthContext);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthContext.Provider value={""}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload. {authContext}
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </AuthContext.Provider>
   );
 }
 
