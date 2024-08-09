6026ca7839a2e1d2be81bcfc1ffe3e649f3c5515
Cheena <c.bazarbai@gmail.com>
2024-08-09 19:20:25 +0500

    feat: add User component

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,3 +1,29 @@
+import { useContext } from "react";
+import "../css/User.css";
+import { AuthContext } from "./AuthContext";
+
 export default function User() {
-  return <div>User component</div>;
+  const authContext = useContext(AuthContext);
+
+  // TODO: fetch user data using localforage
+
+  if (authContext.length === 0) {
+    // TODO: redirect to login
+    console.error("wait u shouldnt be here");
+  }
+
+  return (
+    <div className="user">
+      <section>
+        <h1>User info</h1>
+
+        <p>Username: </p>
+        <p>Email: </p>
+        <p>Phone: </p>
+        <p>Address: </p>
+      </section>
+      {/* TODO: add logout functionality */}
+      <button>Logout</button>
+    </div>
+  );
 }
